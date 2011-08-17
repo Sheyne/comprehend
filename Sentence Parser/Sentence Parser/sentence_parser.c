36997ff5c1b2ee0785b10bacb4b22d599a486e98
Sheyne <me@sheyne.com>
2011-08-17 09:11:03 -0600

    Final Commit for me implementing my own OOP.

--- Sentence Parser/Sentence Parser/sentence_parser.c
+++ Sentence Parser/Sentence Parser/sentence_parser.c
@@ -6,16 +6,36 @@
 //  Copyright 2011 Sheyne Anderson. All rights reserved.
 //
 
+#include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <assert.h>
 #include "sentence_parser.h"
 
-void SPRetain(sp_base obj){
-	obj->ref_count++;
-}
-void SPRelease(sp_base obj){
-	assert(obj->ref_count>0);
-	if (!--obj->ref_count) {
-		free(obj);
+#define SPRetain(obj) obj;++obj->ref_count
+#define SPRelease(obj) assert(obj->ref_count>0);\
+	if (!--obj->ref_count) {\
+		obj->dealloc(obj);\
+		free(obj);\
+		obj=NULL;\
 	}
+
+SPClassImplement(sp_type, sp_string type,{
+	self->type=SPRetain(type);
+},{
+	SPRelease(self->type);
+})
+SPClassImplement(sp_string, char *value,{
+	self->value=malloc(strlen(value));
+	strcpy(self->value, value);
+},{
+	free(self->value);
+})
+void function(void);
+
+void function(void){
+	sp_string str=sp_string_alloc();
+	str->init(str,"noun");
+	sp_type type=sp_type_alloc();
+	type->init(type,str);
 }
--- Sentence Parser/Sentence Parser/sentence_parser.h
+++ Sentence Parser/Sentence Parser/sentence_parser.h
@@ -10,33 +10,34 @@
 #define Sentence_Parser_sentence_parser_h
 
 
-/*abstract base type for all classes to inherit from, all sp_classes inherit by prefixing
- their declairation with struct sp_base super.*/
+#define SPClassImplement(name, args, init_, dealloc_) \
+void name##_init(name self, args){init_}\
+void name##_dealloc(name self){dealloc_}\
+name name##_alloc(){\
+name self=malloc(sizeof(struct name));\
+self->dealloc=name##_dealloc;\
+self->init=name##_init;\
+return self;\
+}
 
-#define SPClass(_name,_base, _content) struct _name{struct _base super;_content};\
-typedef struct _name *_name
+#define SPClass(_name,_base, args, _content) \
+struct _name;\
+typedef struct _name *_name;\
+struct _name{struct _base super;int ref_count;void (*dealloc)(_name self);void (*init)(_name self, args);_content};\
+void _name##_dealloc(_name self) ; \
+void _name##_init(_name self, args); \
+_name _name##_alloc(void);
 
-SPClass(sp_base,,
-		int ref_count;
-		);
 
-SPClass(sp_type, sp_base,
-		char*type;
-		);
-SPClass(sp_punctuation, sp_base,
-		char punctuation;
-		);
-SPClass(sp_word, sp_base,
-		char *word;
-		sp_type *types;
-		);
+SPClass(sp_base,{},void *unused,)
+SPClass(sp_string, sp_base, char *value, char*value;)
+SPClass(sp_type, sp_base,sp_string type,sp_string type;)
+SPClass(sp_punctuation, sp_base,char punctuation,char punctuation;)
+SPClass(sp_word, sp_base,char *word,char *word;sp_type *types;)
 
 void SPRetain(sp_base obj);
 void SPRelease(sp_base obj);
 
-sp_type SPMakeType(char *type);
-sp_word SPWordType(char *word, sp_type types[0]);
-sp_punctuation SPMakePunctuation(char punctuation);
 
 typedef sp_base sp_sentence[0];
 /* An array of sp_words terminated by a sp_punctuation. */
