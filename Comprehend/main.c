b24357cd025304a3f0d0cd650b4651d3adca0ead
Sheyne <me@sheyne.com>
2011-08-27 22:55:01 -0600

    Add Xcode project.

---
+++ Comprehend/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  Comprehend
+//
+//  Created by Sheyne Anderson on 8/27/11.
+//  Copyright 2011 Sheyne Anderson. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main (int argc, const char * argv[])
+{
+
+	// insert code here...
+	printf("Hello, World!\n");
+    return 0;
+}
+
