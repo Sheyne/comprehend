dfaf368f288e62d1e3085d2cab4ed5bcf004f0ce
Sheyne <me@sheyne.com>
2011-08-28 20:47:03 -0600

    Moving files...

---
+++ Comprehend/api/comprehend.c
@@ -0,0 +1,10 @@
+//
+//  comprehend.c
+//  Comprehend
+//
+//  Created by Sheyne Anderson on 8/28/11.
+//  Copyright 2011 Sheyne Anderson. All rights reserved.
+//
+
+#include <stdio.h>
+#include "comprehend.h"
---
+++ Comprehend/api/comprehend.h
@@ -0,0 +1,14 @@
+//
+//  comprehend.h
+//  Comprehend
+//
+//  Created by Sheyne Anderson on 8/28/11.
+//  Copyright 2011 Sheyne Anderson. All rights reserved.
+//
+
+#ifndef Comprehend_comprehend_h
+#define Comprehend_comprehend_h
+
+
+
+#endif
