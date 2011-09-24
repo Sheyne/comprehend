bd4ca78f3003037cd8db5a3068ed99e790b34f42
Sheyne <me@sheyne.com>
2011-09-23 20:35:05 -0600

    Added to comprehend.

--- Comprehend/source/test.py
+++ Comprehend/source/test.py
@@ -1,20 +1,3 @@
-import linkgrammar
 import comprehend
 
-dict=linkgrammar.Dictionary("en")
-sent = dict.Sentence("The dog has brown fur.")
-
-for linkage in sent.linkages:
-	print linkage
-	subject = None
-	object = None
-	verb = None
-	for link in linkage.links:
-		if link.type.mayjor == "S":
-			subject=link.left
-			verb=link.right
-	for link,direction in subject.links:
-		if not direction and link.type.mayjor=="D":
-			subject=comprehend.Phrase((link.right, link.left,), link.right)
-	print subject, verb
-	break
+comprehend.process_sentence("The dog has brown fur.")
