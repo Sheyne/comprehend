e1dbde9211beed9944673f0ae5b429973044f27c
Sheyne Anderson <me@sheyne.com>
2012-01-31 00:22:53 -0700

    Modified unit tests

--- parse/parse.py
+++ parse/parse.py
@@ -6,9 +6,9 @@ import sys
 import os
 from datetime import datetime
 
-dictionary = mindnode.MindMap(os.path.expanduser('~/dev/misc/comprehend/parseMindNode/dictionary.mindnode'))
+dictionary = mindnode.MindMap(os.path.expanduser('dictionary.mindnode'))
 
-queryname = os.path.expanduser('~/dev/misc/comprehend/parseMindNode/queries/test1.mindnode')
+queryname = os.path.expanduser('queries/test1.mindnode')
 
 query = graph.Query(mindnode.MindMap(queryname))
 
@@ -18,7 +18,7 @@ date of start: %s
 """ % (datetime.now(), )
 
 for solution in query.match(dictionary):
-	print solution
+	print "the real solution is:",solution
 
 
 a = graph.AnonymousNode()
@@ -27,9 +27,12 @@ c = graph.AnonymousNode()
 
 l = [(a, "hi"), ("hi", b)]
 
-l2 = graph.replace_node(l,b, c)
+l2 = graph.replace_node(l, b, c)
+print "printing l2"
 print l2
 
+print "printing 1"
 print graph.matching_edges(graph.compare_edges,l,("hi",b))
+print "printing 2"
 print graph.matching_edges(graph.compare_edges,l2,("hi",b))
 
