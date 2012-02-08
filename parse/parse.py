71750c89c96a24b8ff95f1d60e2b0828cd82c3fd
Sheyne Anderson <me@sheyne.com>
2012-02-08 11:04:43 -0700

    Cleaned up the mess that was the file structure.

--- parse/parse.py
+++ parse/parse.py
@@ -1,22 +1,22 @@
 #! /usr/bin/env python
 
 import graph
-import mindnode
+import graph.mindnode
 
 # load a graph from a mindnode type graph
-dictionary = mindnode.mindmap('dictionary.mindnode')
+dictionary = graph.mindnode.mindmap('resources/dictionary.mindnode')
 
 # dump the graph into 'dictionary.edgepairgraph'
-dictionary.dump(open("dictionary.edgepairgraph","w"))
+dictionary.dump(open("resources/dictionary.edgepairgraph","w"))
 
 # load another graph from a mindnode type graph. 
-enviroment = mindnode.mindmap('enviroment.mindnode')
+enviroment = graph.mindnode.mindmap('resources/enviroment.mindnode')
 
 # merge dictionary into the enviroment
 enviroment.union(dictionary)
 
 # load another graph from a mindnode type graph. 
-query_dictionary = mindnode.mindmap('queries/test1.mindnode')
+query_dictionary = graph.mindnode.mindmap('resources/queries/test1.mindnode')
 # this graph will be used as a query. 
 
 # run the query on the envieroment
