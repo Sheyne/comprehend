5cd3e0745a7817d8ec72330383a7194dd0af2cf5
Sheyne Anderson <me@sheyne.com>
2012-02-13 10:54:25 -0700

    Standardized parse
    to compare __name__ to "__main__" and run a main function.

--- parse/parse.py
+++ parse/parse.py
@@ -3,28 +3,32 @@
 import graph
 import graph.mindnode
 
-# jump into the dir of the script (xcode launches from some wierd place. 
-import os
-os.chdir(os.path.dirname(__file__))
+def main():
+    # load a graph from a mindnode type graph
+    dictionary = graph.mindnode.mindmap('resources/dictionary.mindnode')
+    graph.mindnode.write(dictionary, "test1.mindnode")
 
-# load a graph from a mindnode type graph
-dictionary = graph.mindnode.mindmap('resources/dictionary.mindnode')
-graph.mindnode.write(dictionary, "test1.mindnode")
+    # dump the graph into 'dictionary.edgepairgraph'
+    dictionary.dump(open("resources/dictionary.edgepairgraph","w"))
 
-# dump the graph into 'dictionary.edgepairgraph'
-dictionary.dump(open("resources/dictionary.edgepairgraph","w"))
+    # load another graph from a mindnode type graph. 
+    enviroment = graph.mindnode.mindmap('resources/enviroment.mindnode')
 
-# load another graph from a mindnode type graph. 
-enviroment = graph.mindnode.mindmap('resources/enviroment.mindnode')
+    # merge dictionary into the enviroment
+    enviroment.union(dictionary)
 
-# merge dictionary into the enviroment
-enviroment.union(dictionary)
+    # load another graph from a mindnode type graph. 
+    query_dictionary = graph.mindnode.mindmap('resources/queries/test1.mindnode')
+    # this graph will be used as a query. 
 
-# load another graph from a mindnode type graph. 
-query_dictionary = graph.mindnode.mindmap('resources/queries/test1.mindnode')
-# this graph will be used as a query. 
+    # run the query on the envieroment
+    for result in enviroment.query(query_dictionary):
+        # print out result dictionaries. 
+        print result
 
-# run the query on the envieroment
-for result in enviroment.query(query_dictionary):
-	# print out result dictionaries. 
-	print result
+
+if __name__ == "__main__":
+    # jump into the dir of the script (xcode launches from some wierd place. 
+    import os
+    os.chdir(os.path.dirname(__file__))
+    main()
