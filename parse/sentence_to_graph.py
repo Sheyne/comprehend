9323389cbfd3622f97a1d5dcdd08e1c02a9fd9c7
Sheyne Anderson <me@sheyne.com>
2012-03-12 08:22:24 -0600

    Removed some debug messages, and changed up the sentences.

--- parse/sentence_to_graph.py
+++ parse/sentence_to_graph.py
@@ -4,7 +4,7 @@ class Meanings(WordMeanings):
 	pretable = """
 	what the a: determiner
 	dog cat color: noun
-	red quickly brown: adj
+	red quickly brown blue: adj
 	chases is: verb
 	"""
 	def noun(self, word):
@@ -14,34 +14,24 @@ class Meanings(WordMeanings):
 		
 	def verb(self, word):
 		instance = self.instantiate(word)
-		print "hop"
 		yield Store("verb instance",instance)
-		print "hop1"
 		yield Store("active", instance)	
-		print "hop2"
 
 		noun1 = yield Pull("noun instance", alreay_in_dict=True)
-		print "hop3"
 		noun2 = yield Pull("noun instance", alreay_in_dict=False, optional=True)
-		print "hop4"
 
 		self.graph.add(noun1, instance)
 		if noun2:
 			self.graph.add(instance, noun2)
 			
 	def determiner(self, word):
-		print ":("
 		yield Store("active", None)
 		noun_instance = yield Pull("noun instance", alreay_in_dict=False)
 		self.graph.add(word, noun_instance)
-		print "yo"
-		print "yo1"
 		
 	def adj(self, word):		
 		instance = self.instantiate(word)
-		print "ll"
 		active = yield Pull("active")
-		print '111'
 		#if self.graph.has_edges(active, "*", "noun"):
 		#	print "oh la la, its a noun"
 		self.graph.add(active, instance)
@@ -60,5 +50,5 @@ def sentence_to_graph(sentence):
 if __name__ == "__main__":
 	from magicate import prettify
 	
-	g = sentence_to_graph("what color is the red dog")
+	g = sentence_to_graph("the dog chases the blue cat")
 	print prettify(g)
