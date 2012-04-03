ec26ccae7e15cbb9abc5c994dd119c47ac4b610f
Sheyne Anderson <me@sheyne.com>
2012-04-03 15:38:53 -0600

    Added binary parser

--- parse/sentence_to_graph.py
+++ parse/sentence_to_graph.py
@@ -66,7 +66,7 @@ class Meanings(WordMeanings):
 		self.graph.add(active, instance)
 
 
-def sentence_to_graph(sentence):
+def parse_sentence(sentence):
 	wm = Meanings()
 	d = Datastore()
 	for word in sentence.split(" "): # use better splitting function later
@@ -89,45 +89,3 @@ def prev_noun_instance(optional=False):
 
 def get_tag():
 	return Pull("lookup", alreay_in_dict=True, optional=True)
-	
-if __name__ == "__main__":
-	import mindnode
-	from magicate import prettify
-	
-	dictionary = mindnode.mindmap('resources/dictionary.mindnode')
-	
-	database = graphmodule.Graph()
-	database.mutate(dictionary)
-	
-	last_query = None
-	
-	while True:
-		command = raw_input(": ").lower()
-		if "?" == command:
-			query = graphmodule.Graph()
-			while True:
-				line = raw_input("? ")
-				if line == "":
-					break
-				else:
-					query.add(*line.split(" "))
-			print database.query(query)		
-		elif "exit" == command:
-			break
-		elif ">" == command:
-			while True:
-				line = raw_input("> ")
-				if line == "":
-					break
-				words = line.split(" ")
-				if last_query is not None:
-					words = tuple(last_query[word] if word in last_query else word for word in words)
-				database.add(*words)
-		else:
-			try:
-				g = sentence_to_graph(command.replace(".", ""))
-			except WordNotFound as w:
-				print "I do not know the word \"%s\"" % w
-			else:
-				g.mutate(dictionary)
-				database.add_info(g)		
